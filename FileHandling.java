4fc3e5f666aefbc97b179cac9f20514404260587
Anand5329 <46213999+Anand5329@users.noreply.github.com>
2019-03-29 12:22:54 +0530

    added File Handling

--- FileHandling.java
+++ FileHandling.java
@@ -1,10 +1,16 @@
 package ClinicSoftware;
 import java.io.*;
+import com.opencsv.*;
+
 import java.util.*;
 public class FileHandling {
-    void createFile(String fileName)throws IOException
+    String recordHeader[]={"Name","Phone No.","First Date","Age","Latest Date","Description","Money","Heart Condition","Allergy","Diabetes","Blood Pressure"};
+
+    void createRecordFile()throws IOException
     {
-        FileWriter fw=new FileWriter(fileName);
-        
+        FileWriter fw=new FileWriter("Record.csv");
+        CSVWriter writer=new CSVWriter(fw);
+        writer.writeNext(recordHeader);
+        writer.close();
     }
 }
