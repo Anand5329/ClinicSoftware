93e36ee3b71458566ccec64f250942ffdf99c3b3
Anand5329 <46213999+Anand5329@users.noreply.github.com>
2019-03-29 22:08:28 +0530

    everything fixed(hopefully working too)

--- FileHandling.java
+++ FileHandling.java
@@ -10,53 +10,71 @@ public class FileHandling {
     private String recordHeader[]={"Name","Phone No.","Age","First Date","Latest Date","Description","Money","Heart Condition","Allergy","Diabetes","Blood Pressure"};
     private String labHeader[]={"Sent Date","Received Date","Lab Name","Description"};
     private String prescriptionHeader[]={"Date","Medicine Name","Instruction"};
+    private String dir="C:/Anand/Code Projects!/Directories/";
 
     void createRecordFile(Record patient)throws IOException
     {
-        FileWriter fw=new FileWriter("D:/Java-Blue J/src/ClinicSoftware/Records/"+patient.getName()+"\t"+patient.getPhone()+".csv");
+        FileWriter fw=new FileWriter(dir+"Records/"+patient.getName()+" "+patient.getPhone()+".csv");
         CSVWriter writer=new CSVWriter(fw);
         writer.writeNext(recordHeader);
         String recordDetails[]={patient.getName(),""+patient.getPhone(),""+patient.getAge(),patient.getFirstDate(),patient.getLatestDate(),patient.getDesc(),patient.getMoney()+"",patient.getHeartCondition()+"",patient.getAllergy()+"",patient.getDiabetes()+"",patient.getBloodPressure()+""};
         writer.writeNext(recordDetails);
         writer.close();
+        fw.close();
     }
 
     void createLabFile(LabWork lab)throws IOException
     {
-        FileWriter fw =new FileWriter("D:/Java-Blue J/src/ClinicSoftware/Lab Work/"+lab.getPatientName()+"\t"+lab.getSentDate()+".csv");
+        FileWriter fw =new FileWriter(dir+"Lab Work/"+lab.getPatientName()+" "+lab.getSentDate()+".csv");
         CSVWriter writer=new CSVWriter(fw);
         writer.writeNext(labHeader);
-        String labDetails[]={lab.getSentDate(),lab.getReceivedDate(),lab.getLabName(),lab.getWork()}
+        String labDetails[]={lab.getSentDate(),lab.getReceivedDate(),lab.getLabName(),lab.getWork()};
         writer.writeNext(labDetails);
         writer.close();
+        fw.close();
     }
 
     void createPrescriptionFile(Prescription prescription)throws IOException
     {
-        FileWriter fw = new FileWriter("D:/Java-Blue J/src/ClinicSoftware/Lab Work/" + prescription.getPatientName() + "\t" + prescription.getDate() + ".csv");
+        FileWriter fw = new FileWriter(dir+"Prescriptions/" + prescription.getPatientName() + " " + prescription.getDate() + ".csv");
         CSVWriter writer = new CSVWriter(fw);
         writer.writeNext(prescriptionHeader);
-        String prescriptionDetails[]={prescription.getDate(),prescription.getMedicines(),prescription.getInstruction()};
+        String prescriptionDetails[]={prescription.getDate()};
+
         writer.writeNext(prescriptionDetails);
+        for(int i=0;i<prescription.getMedicines().size();i++)
+        {
+            String temp[]={"",prescription.getMedicines().get(i),prescription.getInstruction().get(i)};
+            writer.writeNext(temp);
+        }
         writer.close();
+        fw.close();
     }
 
     void createAppointmentFile(Appointment a)throws IOException
     {
-        FileWriter fw=new FileWriter("C:/Anand/Code Projects!/Appointments/"+a.getPatient().getName()+"\t"+a.getDate()+".csv");
+        FileWriter fw=new FileWriter(dir+"Appointments/"+a.getPatient().getName()+" "+a.getDate()+".csv");
         CSVWriter writer=new CSVWriter(fw);
         writer.writeNext(appointmentHeader);
-        String[] dat={a.getPatient().getFileName(),a.getDate(),a.getProcedure(),""+a.getPrice(),a.getLab().getPatientName()+"\t"+a.getLab().getSentDate()+".csv",a.getTime().displaySlot(),a.getPrescription().getPatientName()+"\t"+a.getPrescription().getDate()+".csv"};
+        String[] dat={a.getPatient().getFileName(),a.getDate(),a.getProcedure(),""+a.getPrice(),a.getLab().getPatientName()+" "+a.getLab().getSentDate()+".csv",a.getTime().displaySlot(),a.getPrescription().getPatientName()+" "+a.getPrescription().getDate()+".csv"};
         writer.writeNext(dat);
         writer.close();
+        fw.close();
     }
 
     void createScheduleFile(Schedule s)throws IOException
     {
-        FileWriter fw=new FileWriter("C:/Anand/Code Projects!/Schedules/"+s.getDate()+".csv");
+        FileWriter fw=new FileWriter(dir+"Schedules/"+s.getDate()+".csv");
         CSVWriter writer=new CSVWriter(fw);
         writer.writeNext(scheduleHeader);
+        for(int i=0;i<s.getSlots().size();i++)
+        {
+            String entry[]={s.getSlots().get(i).displaySlot(),"Appointments/"+s.getAppointments().get(i).getPatient().getName()+" "+s.getAppointments().get(i).getDate()+".csv"};
+            writer.writeNext(entry);
+        }
         writer.close();
+        fw.close();
     }
+
 
 }
