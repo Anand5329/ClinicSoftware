25208bbc73d6c5fa27b3263912e7ad3364ca4e85
Anand5329 <46213999+Anand5329@users.noreply.github.com>
2019-04-15 20:29:25 +0530

    javafx now working

--- first.java
+++ first.java
@@ -1,38 +1,52 @@
 package ClinicSoftware;
 
 import javafx.application.Application;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import java.awt.*;
-
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 public class first extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
     public void start(Stage primaryStage) {
-        Button btn1=new Button("Hello World");
-        btn1.setOnAction(new EventHandler<ActionEvent>(){
-            @Override
-            public void handle(ActionEvent arg)
-            {
-                System.out.println("Hello World");
-            }
-        });
+        try {
+            Button btn = new Button();
+            btn.setText("Say 'Hello World'");
+            btn.setOnAction(new EventHandler<ActionEvent>() {
 
-        StackPane root=new StackPane();
-        root.getChildren().add(btn1);
-        Scene scene=new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Button");
-        primaryStage.show();
+                @Override
+                public void handle(ActionEvent event) {
+                    System.out.println("Hello World!");
+                }
+            });
+
+            StackPane root = new StackPane();
+            root.getChildren().add(btn);
+
+            Scene scene = new Scene(root, 300, 250);
+
+            primaryStage.setTitle("Hello World!");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
     }
 
+
+    public static void main(String[] args) {
+        try {
+            launch(args);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
 }
