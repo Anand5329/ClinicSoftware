0b1693733bd5608d7e78b9fb25a1173d91790364
Anand5329 <46213999+Anand5329@users.noreply.github.com>
2019-05-29 20:50:31 +0530

    added two-digit format

--- MyDate.java
+++ MyDate.java
@@ -10,11 +10,26 @@ public class MyDate {
     public MyDate()
     {
         Date date=new Date();
+        this.date=getDate(date);
+    }
+
+    String[] getDate(Date date)
+    {
         String arr[]=date.toString().split(" ");
-        this.date[0]=arr[2];
+        int d=Integer.valueOf(arr[2]);
         int m=getMonthNumber(arr[1]);
-        this.date[1]=Integer.toString(m);
-        this.date[2]=arr[5];
+        arr[1]=Integer.toString(m);
+        int y=Integer.valueOf(arr[5]);
+        String myDate[]={""+d,""+m,""+y};
+
+        if(noOfDigits(d)==1)
+            myDate[0]="0"+myDate[2];
+        if(noOfDigits(m)==1)
+            myDate[1]="0"+myDate[1];
+        if(noOfDigits(y)==1)
+            myDate[2]="0"+myDate[5];
+
+        return myDate;
     }
 
     private int getMonthNumber(String monthName) {
@@ -24,5 +39,16 @@ public class MyDate {
     public String toString()
     {
         return date[0]+"-"+date[1]+"-"+date[2];
+    }
+
+    int noOfDigits(int n)
+    {
+        int ctr=0;
+        while(n>0)
+        {
+            n=n/10;
+            ctr++;
+        }
+        return ctr;
     }
 }
